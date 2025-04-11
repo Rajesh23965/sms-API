81066a397be0906695cb828ddc0e68482c0c18de
sanjay <kumarsanjay00128@gmail.com>
2025-04-11 16:53:33 +0545

    new changes

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
+
+// Middleware and settings
 app.use(cors());
 app.use(express.json());
+app.set("view engine", "ejs");
 
+// Import and use routes
+const loginRoute = require("./routes/loginRoute");
+app.use("/", loginRoute); // Now /login works
 
-app.get("/", async (req, res) => {
-    res.send("Hello")
-})
-
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 
 module.exports = app;
